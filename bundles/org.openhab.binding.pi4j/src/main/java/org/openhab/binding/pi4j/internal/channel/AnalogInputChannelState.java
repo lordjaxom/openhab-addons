ab3555d5b5416b04eb5cdc78b0b331fa53e69d07
Sascha Volkenandt <sascha@akv-soft.de>
2023-01-14 22:55:55 +0100

    License, Spotless

--- bundles/org.openhab.binding.pi4j/src/main/java/org/openhab/binding/pi4j/internal/channel/AnalogInputChannelState.java
+++ bundles/org.openhab.binding.pi4j/src/main/java/org/openhab/binding/pi4j/internal/channel/AnalogInputChannelState.java
@@ -1,17 +1,34 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.binding.pi4j.internal.channel;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.pi4j.internal.device.GpioProviderDevice;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.thing.Channel;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.RefreshType;
 
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinAnalogInput;
 import com.pi4j.io.gpio.GpioProvider;
 import com.pi4j.io.gpio.event.GpioPinListenerAnalog;
-import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.binding.pi4j.internal.device.GpioProviderDevice;
-import org.openhab.core.library.types.DecimalType;
-import org.openhab.core.library.types.OpenClosedType;
-import org.openhab.core.thing.Channel;
-import org.openhab.core.types.Command;
-import org.openhab.core.types.RefreshType;
 
+/**
+ * The {@link AnalogInputChannelState}.
+ *
+ * @author Sascha Volkenandt - Initial contribution
+ */
 @NonNullByDefault
 public class AnalogInputChannelState extends BaseChannelState {
 
