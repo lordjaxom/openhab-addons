63aace79db2a4c262d219c399cc1e3d2c1708ee6
Sascha Volkenandt <sascha@akv-soft.de>
2023-01-14 14:23:32 +0100

    Implement generator to introspect available Pi4J devices

---
+++ bundles/org.openhab.binding.pi4j/src/test/java/org/openhab/binding/pi4j/internal/generator/Device.java
@@ -0,0 +1,97 @@
+package org.openhab.binding.pi4j.internal.generator;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Optional;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+import com.pi4j.io.gpio.GpioProvider;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.i2c.I2CBus;
+
+@NonNullByDefault
+public class Device {
+
+    private final Class<? extends GpioProvider> provider;
+    private final Type type;
+    private final String id;
+    private final String name;
+    private final Field pinsField;
+    private final Pin[] pins;
+
+    public Device(Class<? extends GpioProvider> provider, Type type, String id, String name, Field pinsField,
+            Pin[] pins) {
+        this.provider = provider;
+        this.type = type;
+        this.id = id;
+        this.name = name;
+        this.pinsField = pinsField;
+        this.pins = pins;
+    }
+
+    public String getProviderName() {
+        return provider.getName();
+    }
+
+    public String getProviderSimpleName() {
+        return provider.getSimpleName();
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getJavaId() {
+        return id.toUpperCase(Locale.ROOT);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPinClassName() {
+        return pinsField.getDeclaringClass().getName();
+    }
+
+    public String getPinsFieldName() {
+        return pinsField.getDeclaringClass().getSimpleName() + "." + pinsField.getName();
+    }
+
+    public Pin[] getPins() {
+        return pins;
+    }
+
+    @NonNullByDefault
+    public enum Type {
+        I2C("I²C") {
+            @Override
+            boolean isOfType(Class<? extends GpioProvider> clazz) {
+                return Arrays.stream(clazz.getConstructors())
+                        .anyMatch(constructor -> constructor.getParameterTypes().length > 0
+                                && constructor.getParameterTypes()[0] == I2CBus.class);
+            }
+        };
+
+        public static Optional<Type> fromGpioProviderClass(Class<? extends GpioProvider> clazz) {
+            return Arrays.stream(values()).filter(value -> value.isOfType(clazz)).findFirst();
+        }
+
+        private final String name;
+
+        Type(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        abstract boolean isOfType(Class<? extends GpioProvider> clazz);
+    }
+}
---
+++ bundles/org.openhab.binding.pi4j/src/test/java/org/openhab/binding/pi4j/internal/generator/DeviceBindingsBuilder.java
@@ -0,0 +1,46 @@
+package org.openhab.binding.pi4j.internal.generator;
+
+import static org.openhab.binding.pi4j.internal.generator.Pi4JGenerator.findSupportedPinModes;
+import static org.openhab.binding.pi4j.internal.generator.Pi4JGenerator.findSupportedPullResistances;
+import static org.openhab.binding.pi4j.internal.generator.Pi4JGenerator.typeId;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinPullResistance;
+
+@NonNullByDefault
+public class DeviceBindingsBuilder {
+
+    public static Map<String, ?> build(Device device) {
+        return Map.of("device", device, "channels", buildChannels(device));
+    }
+
+    private static List<Map<String, ?>> buildChannels(Device device) {
+        return findSupportedPinModes(device).map(pinMode -> buildChannel(device, pinMode))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static Map<String, ?> buildChannel(Device device, PinMode pinMode) {
+        return Map.of("type", typeId(pinMode), "name", device.getId() + "_" + typeId(pinMode), "pins",
+                buildChannelPins(device, pinMode), "pullModes", buildChannelPullModes(device, pinMode));
+    }
+
+    private static List<Pin> buildChannelPins(Device device, PinMode pinMode) {
+        return Arrays.stream(device.getPins()).filter(pin -> pin.getSupportedPinModes().contains(pinMode))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static List<PinPullResistance> buildChannelPullModes(Device device, PinMode pinMode) {
+        if (pinMode == PinMode.DIGITAL_INPUT) {
+            return findSupportedPullResistances(device).collect(Collectors.toUnmodifiableList());
+        }
+        return List.of();
+    }
+}
